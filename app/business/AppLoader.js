b78de0b99e0f582e56c5399e89ee911d3b4aa96f
miaojian <kitemao@gmail.com>
2014-03-27 12:44:49 +0800

    test travis ci

--- app/business/AppLoader.js
+++ app/business/AppLoader.js
@@ -3,7 +3,7 @@ require(['RequireConfig', 'app'], function () {
     var angular = require('angular');
 
     // 如果search包含e2e，则为e2e测试
-    if ( window.location.search.indexOf('e2e') !== -1) {
+    if (window.location.search.indexOf('e2e') !== -1) {
         
         require(['/e2e-mocks.js'], function () {
             
