967face6fae965f455130a3f7fb79a36288649f4
miaojian <kitemao@gmail.com>
2014-04-14 21:04:16 +0800

    login auth logic

---
+++ app/business/main/services/AccountDaoService.js
@@ -0,0 +1,29 @@
+(function (window) {
+    define([ '_' ], function (_) {
+        var AccountDaoService = function ($resource, $rootScope, CONFIG) {
+
+            var actions = {
+                login: 'account/auth',
+                logout: 'account/logout'
+            };
+
+            for (var key in actions) {
+                if (actions.hasOwnProperty(key)) {
+                    actions[key] = CONFIG.API_URL_PREFIX + actions[key];
+                }
+            }
+
+            return {
+                login: $resource(actions.login),
+
+                logout: $resource(actions.logout),
+
+                user: $resource('https://www.googleapis.com/plus/v1/people/me')
+            };
+        };
+
+        AccountDaoService.$inject = [ '$resource', '$rootScope', 'CONFIG' ];
+
+        return AccountDaoService;
+    });
+}(this));
