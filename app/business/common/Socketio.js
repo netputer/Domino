e6cfc44bec7ca9abda504b9f4f88101eb64136dd
Wangye Zhao | 赵望野 <wangye.zhao@wandoujia.com>
2014-04-21 17:04:42 +0800

    Fix socket.io config.

--- app/business/common/Socketio.js
+++ app/business/common/Socketio.js
@@ -12,46 +12,41 @@ define([
     'use strict';
 
     angular.module('ioModule', [])
+        .factory('socket', ['$rootScope', '$timeout', 'CONFIG',
+            function ($rootScope, $timeout, CONFIG) {
+                var socket;
 
-    .factory('socket', [ '$rootScope', '$timeout', 'CONFIG',
+                function createSocket() {
+                    // TODO 连接成功时，可能页面还没有初始化完毕，page intialize时
+                    socket = io.connect(CONFIG.API_URL_PREFIX, {
+                        transports: ['websocket', 'polling'],
+                        resource: window.API_URL_PREFIX ? 'socket.io' : 'api/socket.io'
+                    });
 
-        function ($rootScope, $timeout, CONFIG) {
-
-            var socket;
-
-            function createSocket() {
-                // TODO 连接成功时，可能页面还没有初始化完毕，page intialize时
-                socket = io.connect(CONFIG.API_URL_PREFIX, {
-
-                    transports : [ 'websocket', 'polling' ]
-                });
-
-                socket.on('connect', function () {
-                    console.log('socket connect success');
+                    socket.on('connect', function () {
+                        console.log('socket connect success');
+                    });
 
                     socket.on('disconnect', function () {
-
                         console.log('socket has disconnect');
                     });
-                });
-            }
+                }
 
-            function addEventName() {
-                _.forEach(arguments, function (name) {
-                    socket.on(name, function (data) {
-                        $rootScope.$broadcast('io.' + name, data);
+                function addEventName() {
+                    _.forEach(arguments, function (name) {
+                        socket.on(name, function (data) {
+                            $rootScope.$broadcast('io.' + name, data);
+                        });
                     });
-                });
+                }
+
+                return {
+                    init: createSocket,
+                    addEventName: addEventName,
+                    io: socket
+                };
             }
-
-            return {
-                init: createSocket,
-                addEventName: addEventName,
-                io: socket
-            };
-        }
-    ]).run(['socket', function (socket) {
-
-        socket.init();
-    }]);
+        ]).run(['socket', function (socket) {
+            socket.init();
+        }]);
 });
